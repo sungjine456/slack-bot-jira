168d64565b2c2432a6c41e16688c60c56e447be6
DESKTOP-CUU6PMC\sungjin <sungjine456@gmail.com>
2018-05-25 22:53:51 +0900

    Jira 클래스에서 URI를 넘기도록 수정

--- src/main/scala/AppLauncher.scala
+++ src/main/scala/AppLauncher.scala
@@ -1,18 +1,11 @@
-import scala.concurrent.Await
-import scala.concurrent.duration._
-import scala.util.{ Failure, Success }
-
 import akka.actor.ActorSystem
-import akka.http.scaladsl.unmarshalling.Unmarshal
-import akka.stream.ActorMaterializer
 import jira.Jira
 import slack.rtm.SlackRtmClient
 import utils.ConfigurationReader
 
 object AppLauncher extends App {
   implicit val system = ActorSystem("slack")
-  implicit val materializer = ActorMaterializer()
-  implicit val ec = system.dispatcher
+  implicit val executionContext = system.dispatcher
 
   val client = SlackRtmClient(ConfigurationReader("slack.token"))
 
@@ -20,12 +13,7 @@ object AppLauncher extends App {
 
   client.onMessage { message =>
     if (message.text.toLowerCase.contains(Jira.issueKey + "-")) {
-      jira.Run(message.text).onComplete {
-        case Success(res) =>
-          val str = Await.result(Unmarshal(res.entity).to[String], 5.seconds)
-          if (Jira.check(str)) client.sendMessage(message.channel, Jira.issueUri(message.text))
-        case Failure(_) =>
-      }
+      jira.getUri(message.text).map(uri => client.sendMessage(message.channel, uri))
     }
   }
 }
--- src/main/scala/jira/Jira.scala
+++ src/main/scala/jira/Jira.scala
@@ -1,23 +1,44 @@
 package jira
 
-import scala.concurrent.Future
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers.BasicHttpCredentials
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.stream.ActorMaterializer
 import utils.ConfigurationReader
 
 class Jira {
-  def Run(message: String): Future[HttpResponse] = {
+  def getUri(message: String): Option[String] = {
     implicit val system = ActorSystem("jira")
+    implicit val materializer = ActorMaterializer()
     implicit val executionContext = system.dispatcher
 
     val authorization = headers.Authorization(BasicHttpCredentials(ConfigurationReader("jira.user"), ConfigurationReader("jira.pass")))
 
-    Http().singleRequest(
+    var uri: Option[String] = None
+
+    val httpResponse = Http().singleRequest(
       HttpRequest(uri = Jira.searchUri + message, headers = List(authorization))
     )
+    val res = Await.result(httpResponse, 2.seconds)
+    val result = Await.result(Unmarshal(res.entity).to[String], 2.seconds)
+
+    if (check(result)) uri = Some(Jira.issueUri(message))
+
+    uri
+  }
+
+  private def check(str: String): Boolean = {
+    val totalStr = "total\":"
+    val totalLength = totalStr.length
+    val totalValuePosition = str.indexOf(totalStr)
+    val totalValue = str.substring(totalValuePosition + totalLength, totalValuePosition + totalLength + 1)
+
+    totalValue != "0"
   }
 }
 
@@ -27,13 +48,4 @@ object Jira {
   val issueKey: String = ConfigurationReader("jira.issueKey").toLowerCase
 
   def issueUri(issueKey: String): String = baseUri + "browse/" + issueKey.toUpperCase
-
-  def check(str: String): Boolean = {
-    val totalStr = "total\":"
-    val totalLength = totalStr.length
-    val totalValuePosition = str.indexOf(totalStr)
-    val totalValue = str.substring(totalValuePosition + totalLength, totalValuePosition + totalLength + 1)
-
-    totalValue != "0"
-  }
 }
