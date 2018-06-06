c5ec70d15dcdcc2daa74783384e15bc1c8a7c6aa
DESKTOP-CUU6PMC\sungjin <sungjine456@gmail.com>
2018-06-06 13:19:23 +0900

    슬랙으로 메시지 보내는 방법 변경

--- src/main/scala/slack/Slack.scala
+++ src/main/scala/slack/Slack.scala
@@ -2,19 +2,24 @@ package slack
 
 import akka.actor.{ Actor, Props }
 import jira.Jira
+import slack.api.SlackApiClient
 import slack.rtm.SlackRtmClient
 import utils.ConfigurationReader
 
 class Slack extends Actor {
   implicit private val system = context.system
+  implicit private val ec = system.dispatcher
 
-  private val client = SlackRtmClient(ConfigurationReader("slack.token"))
+  private val token = ConfigurationReader("slack.token")
+
+  private val rtmClient = SlackRtmClient(token)
+  private val apiClient = SlackApiClient(token)
   private val jiraActor = system.actorOf(Props(new Jira(self)), "Jira")
 
   override def receive: Receive = {
-    case (channelId: String, uri: String) => client.sendMessage(channelId, uri)
+    case (channelId: String, uri: String) => apiClient.postChatMessage(channelId, uri)
     case SlackState.Receive =>
-      client.onMessage { message =>
+      rtmClient.onMessage { message =>
         if (message.text.toUpperCase.contains(Jira.issueKey + "-")) jiraActor ! (message.text, message.channel)
       }
   }
