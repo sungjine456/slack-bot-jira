ba03e4e1245e520590d7d4db051f74d1127f392c
DESKTOP-CUU6PMC\sungjin <sungjine456@gmail.com>
2018-06-16 11:55:28 +0900

    슬랙으로 보내지는 템플릿에 요약과 상태에 대한 정보 추가

--- src/main/scala/jira/JiraContent.scala
+++ src/main/scala/jira/JiraContent.scala
@@ -1,12 +1,24 @@
 package jira
 
 import slack.models.Attachment
-import spray.json.JsObject
+import spray.json.{ JsArray, JsObject, JsValue }
 
 case class JiraContent(private val jsValue: JsObject) {
   def check: Boolean = jsValue.fields.get("total").map(total => total.toString() != "0").get
 
   def makeAttachments(issueKey: String): Option[Seq[Attachment]] = {
-    Some(Seq(Attachment(text = Some("<" + Jira.issueUri(issueKey) + "|" + issueKey + "> : "))))
+    val (summary, statusName) = jsValue.fields("issues").asInstanceOf[JsArray].elements.headOption.map { value =>
+      val fieldsObj = value.asJsObject.fields("fields").asJsObject.fields
+      val statusObj = fieldsObj("status").asJsObject.fields
+
+      (jsValueConvertString(fieldsObj("summary")), jsValueConvertString(statusObj("name")))
+    }.get
+
+    Some(Seq(Attachment(text = Some("<" + Jira.issueUri(issueKey) + "|" + issueKey + "> : `" + statusName + "` " + summary))))
+  }
+
+  private def jsValueConvertString(value: JsValue) = {
+    val str = value.toString()
+    str.substring(1, str.length - 1)
   }
 }
