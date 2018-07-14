bc1fd67dbe1f1c72b7f8a6d6a9d08714d310620d
DESKTOP-CUU6PMC\sungjin <sungjine456@gmail.com>
2018-07-14 14:09:30 +0900

    슬랙 메시지에 담당자와 변경일 추가

--- src/main/scala/jira/JiraContent.scala
+++ src/main/scala/jira/JiraContent.scala
@@ -1,30 +1,41 @@
 package jira
 
+import org.joda.time.format.{ DateTimeFormat, DateTimeFormatter }
 import slack.models.Attachment
 import spray.json.{ DefaultJsonProtocol, RootJsonFormat }
+import JiraContent.datePattern
 
 case class JiraContent(private val content: Content) {
   def nonEmpty: Boolean = content.total > 0
 
   def makeAttachments(issueKey: String): Attachment = {
     content.issues.headOption.map { issue =>
-      (issue.fields.summary, issue.fields.status.name)
+      val date = datePattern.parseDateTime(issue.fields.updated)
+
+      (issue.fields.summary, issue.fields.status.name, issue.fields.assignee.name, date.toLocalDate)
     } match {
-      case Some((summary, statusName)) =>
-        Attachment(text = Some(s"<${ Jira.issueUri(issueKey) }|$issueKey> : `$statusName` $summary"))
+      case Some((summary, statusName, assigneeName, updated)) =>
+        Attachment(title = Some(s"<${ Jira.issueUri(issueKey) }|$issueKey> : `$statusName` $summary"),
+          text=Some(s"$assigneeName | $updated"))
       case None => Attachment(text = Some(s"<${ Jira.issueUri(issueKey) }|$issueKey>"))
     }
   }
 }
 
+object JiraContent {
+  val datePattern: DateTimeFormatter = DateTimeFormat.forPattern("yyyy-mm-dd'T'HH:mm:ss.000Z")
+}
+
 case class Content(total: Int, issues: Seq[Issues])
 case class Issues(fields: Fields)
-case class Fields(summary: String, status: Status)
+case class Fields(summary: String, status: Status, assignee: Assignee, updated: String)
 case class Status(name: String)
+case class Assignee(name: String)
 
 object MyJsonProtocol extends DefaultJsonProtocol {
+  implicit val assigneeFormat: RootJsonFormat[Assignee] = jsonFormat1(Assignee)
   implicit val statusFormat: RootJsonFormat[Status] = jsonFormat1(Status)
-  implicit val fieldsFormat: RootJsonFormat[Fields] = jsonFormat2(Fields)
+  implicit val fieldsFormat: RootJsonFormat[Fields] = jsonFormat4(Fields)
   implicit val issuesFormat: RootJsonFormat[Issues] = jsonFormat1(Issues)
   implicit val contentFormat: RootJsonFormat[Content] = jsonFormat2(Content)
 }
